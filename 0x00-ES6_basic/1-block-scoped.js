a99189a12b8f2f6bd8c26753c0e66026268849ed
ahmedelamin-hub <ahmedelamin42@gmail.com>
2024-07-22 17:19:34 +0000

    fixing hoisteing issues using let

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    let task = true; // This task is block-scoped to the if statement
+    let task2 = false; // This task2 is block-scoped to the if statement
+  }
+
+  return [task, task2];
+}
+
